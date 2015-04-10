463e435910a4c156919d7e35ccfb4023de528ec4
Alex Newlifer <alex.newlifer@gmail.com>
2015-04-10 14:41:03 +0300

    How about writter?!

---
+++ csvwriter.hpp
@@ -0,0 +1,17 @@
+
+#pramga once
+
+
+template< typename DataT, CharT >
+ICSVWritter {
+    typedef CharT char_type;
+    typedef DataT data_type;
+    typedef std::basic_string< char_type > line_type;
+public:
+
+private:
+    void write_line ( std::size_t, const line_type& ) noexcept(false)
+    {
+
+    }
+};
